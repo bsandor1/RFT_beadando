413da8e3708db9da3c3f56e64a90980ec86cecc0
bsandor1 <35764826+bsandor1@users.noreply.github.com>
2018-05-04 20:13:14 +0200

    Termelő

---
+++ rft/Termelo_fogyaszto/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Termelo_fogyaszto
+{
+
+    class Termelo
+    {
+        int db;
+        public Termelo()
+        {
+
+            this.db = 10;
+        }
+
+
+        public void Termel()
+        {
+
+            for (int i = 0; i < db; i++)
+            {
+                Monitor.Enter(Program.puffer);
+                while (Program.puffer.Count >= 100)
+                {
+                    Monitor.Wait(Program.puffer);
+                }
+                int g = Program.rnd.Next(10000, 80000);
+
+                int sleep = Program.rnd.Next(1, 5);
+                Thread.Sleep(sleep);
+                Program.puffer.Add(g);
+                Monitor.Pulse(Program.puffer);
+                Monitor.Exit(Program.puffer);
+
+            }
+            Console.WriteLine("A {0}-as szál leállt.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Termelo t = new Termelo();
+            Console.ReadLine();
+        }
+    }
+}
