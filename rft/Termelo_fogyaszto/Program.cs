ce30dd90aafa911a0ffc83f412b0bfa32ad6be40
bsandor1 <35764826+bsandor1@users.noreply.github.com>
2018-05-09 13:13:51 +0200

    Fogyasztó

--- rft/Termelo_fogyaszto/Program.cs
+++ rft/Termelo_fogyaszto/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Termelo_fogyaszto
 {
@@ -39,12 +40,77 @@ namespace Termelo_fogyaszto
             Console.WriteLine("A {0}-as szál leállt.");
         }
     }
-    class Program
+    class Fogyaszto
     {
-        static void Main(string[] args)
+        int s;
+
+        public Fogyaszto(ref int s)
         {
-            Termelo t = new Termelo();
-            Console.ReadLine();
+            this.s = s;
+        }
+
+        public void Fogyaszt()
+        {
+
+            while (Program.counter > Program.eddigvolt)
+            {
+                Program.eddigvolt++;
+                Monitor.Enter(Program.puffer);
+                while (Program.puffer.Count == 0)
+                {
+                    Monitor.Wait(Program.puffer);
+                }
+
+                Console.WriteLine(Program.puffer[0]);
+                s += Program.puffer[0];
+                Program.puffer.RemoveAt(0);
+                this.FejreszKiir();
+                int sleep = Program.rnd.Next(3, 8);
+                Thread.Sleep(sleep);
+                Monitor.Pulse(Program.puffer);
+                Monitor.Exit(Program.puffer);
+
+            }
+
+            Console.WriteLine("Fogyasztó szál leállt.");
+        }
+
+        public void FejreszKiir()
+        {
+            if (Program.puffer.Count == 0)
+                Console.Title = "Üres";
+            else
+            {
+                if (Program.puffer.Count >= 100)
+                    Console.Title = Program.puffer.Count.ToString();
+
+                else
+                    Console.Title = Program.puffer.Count.ToString();
+            }
         }
     }
-}
+        class Program
+        {
+            static public List<int> puffer = new List<int>();
+            static public Random rnd = new Random();
+            static public int counter = 10;
+            static public int eddigvolt = 0;
+            static void Main(string[] args)
+            {
+                Termelo t = new Termelo();
+
+                int sum = 0;
+                Fogyaszto f1 = new Fogyaszto(ref sum);
+                Fogyaszto f2 = new Fogyaszto(ref sum);
+                Thread szal1 = new Thread(t.Termel);
+
+                Thread szal2 = new Thread(f1.Fogyaszt);
+                Thread szal3 = new Thread(f2.Fogyaszt);
+                szal1.Start();
+                szal2.Start();
+                szal3.Start();
+                Console.ReadLine();
+            }
+        }
+    }
+
